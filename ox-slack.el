16950562164bb4bf9becfee8e6e1ca8c21f04b4d
Matt Price <matt.price@utoronto.ca>
2018-11-09 21:58:21 -0500

    typos, misformatted package-requires, whitespace
    - whitespace according to indent-buffer
    - other changes according to `https://github.com/melpa/melpa/pull/5771#issuecomment-437504376`

--- ox-slack.el
+++ ox-slack.el
@@ -1,4 +1,4 @@
-;;; ox-slack.el --- SLack Exporter for org-mode -*- lexical-binding: t; -*
+;;; ox-slack.el --- Slack Exporter for org-mode -*- lexical-binding: t; -*
 
 
 ;; Copyright (C) 2018 Matt Price
@@ -6,7 +6,7 @@
 ;; Author: Matt Price
 ;; Keywords: org, slack, outlines
 ;; Package-Version: 0.1
-;; Package-Requires ((org 9.1.4) (ox-gfm))
+;; Package-Requires: ((org 9.1.4) (ox-gfm))
 ;; URL: https://github.com/titaniumbones/ox-slack
 
 ;; This file is not part of GNU Emacs.
@@ -58,147 +58,147 @@
     (src-block . org-slack-src-block)
     (strike-through . org-slack-strike-through)
     (timestamp . org-slack-timestamp)
-      ) )
-
+    ) )
 
 ;; timestamp
 (defun org-slack-timestamp (timestamp contents info)
   "Transcode TIMESTAMP element into Slack format.
 CONTENTS is the timestamp contents. INFO is a plist used as a
 ocmmunications channel."
-  (let*
-      ((value (org-html-plain-text (org-timestamp-translate timestamp) info)))
-    value))
+  (org-html-plain-text (org-timestamp-translate timestamp) info)
+  )
 
 ;; headline
-
 (defun org-slack-headline (headline contents info)
   "Transcode HEADLINE element into Markdown format.
 CONTENTS is the headline contents.  INFO is a plist used as
 a communication channel."
   (unless (org-element-property :footnote-section-p headline)
     (let* ((level (org-export-get-relative-level headline info))
-	   (title (org-export-data (org-element-property :title headline) info))
-	   (todo (and (plist-get info :with-todo-keywords)
-		      (let ((todo (org-element-property :todo-keyword
-							headline)))
-			(and todo (concat (org-export-data todo info) " ")))))
-	   (tags (and (plist-get info :with-tags)
-		      (let ((tag-list (org-export-get-tags headline info)))
-			(and tag-list
-			     (concat "     " (org-make-tag-string tag-list))))))
-	   (priority
-	    (and (plist-get info :with-priority)
-		 (let ((char (org-element-property :priority headline)))
-		   (and char (format "[#%c] " char)))))
-	   ;; Headline text without tags.
-	   (heading (concat todo priority title)))
+           (title (org-export-data (org-element-property :title headline) info))
+           (todo (and (plist-get info :with-todo-keywords)
+                      (let ((todo (org-element-property :todo-keyword
+                                                        headline)))
+                        (and todo (concat (org-export-data todo info) " ")))))
+           (tags (and (plist-get info :with-tags)
+                      (let ((tag-list (org-export-get-tags headline info)))
+                        (and tag-list
+                             (concat "     " (org-make-tag-string tag-list))))))
+           (priority
+            (and (plist-get info :with-priority)
+                 (let ((char (org-element-property :priority headline)))
+                   (and char (format "[#%c] " char)))))
+           ;; Headline text without tags.
+           (heading (concat todo priority title)))
       (format "*%s*\n\n%s" title contents)
-       )))
+      )))
 
-
+;; link
 (defun org-slack-link (link contents info)
   "Transcode LINK object into Markdown format.
-CONTENTS is the link's description.  INFO is a plist used as
-a communication channel."
+  CONTENTS is the link's description.  INFO is a plist used as
+  a communication channel."
   (let ((link-org-files-as-md
-	 (lambda (raw-path)
-	   ;; Treat links to `file.org' as links to `file.md'.
-	   (if (string= ".org" (downcase (file-name-extension raw-path ".")))
-	       (concat (file-name-sans-extension raw-path) ".md")
-	     raw-path)))
-	(type (org-element-property :type link)))
+         (lambda (raw-path)
+           ;; Treat links to `file.org' as links to `file.md'.
+           (if (string= ".org" (downcase (file-name-extension raw-path ".")))
+               (concat (file-name-sans-extension raw-path) ".md")
+             raw-path)))
+        (type (org-element-property :type link)))
     (cond
      ;; Link type is handled by a special function.
      ((org-export-custom-protocol-maybe link contents 'md))
      ((member type '("custom-id" "id" "fuzzy"))
       (let ((destination (if (string= type "fuzzy")
-			     (org-export-resolve-fuzzy-link link info)
-			   (org-export-resolve-id-link link info))))
-	(pcase (org-element-type destination)
-	  (`plain-text			; External file.
-	   (let ((path (funcall link-org-files-as-md destination)))
-	     (if (not contents) (format "%s>" path)
-	       (format "[%s](%s)" contents path))))
-	  (`headline
-	   (format
-	    "[%s](#%s)"
-	    ;; Description.
-	    (cond ((org-string-nw-p contents))
-		  ((org-export-numbered-headline-p destination info)
-		   (mapconcat #'number-to-string
-			      (org-export-get-headline-number destination info)
-			      "."))
-		  (t (org-export-data (org-element-property :title destination)
-				      info)))
-	    ;; Reference.
-	    (or (org-element-property :CUSTOM_ID destination)
-		(org-export-get-reference destination info))))
-	  (_
-	   (let ((description
-		  (or (org-string-nw-p contents)
-		      (let ((number (org-export-get-ordinal destination info)))
-			(cond
-			 ((not number) nil)
-			 ((atom number) (number-to-string number))
-			 (t (mapconcat #'number-to-string number ".")))))))
-	     (when description
-	       (format "[%s](#%s)"
-		       description
-		       (org-export-get-reference destination info))))))))
+                             (org-export-resolve-fuzzy-link link info)
+                           (org-export-resolve-id-link link info))))
+        (pcase (org-element-type destination)
+          (`plain-text			; External file.
+           (let ((path (funcall link-org-files-as-md destination)))
+             (if (not contents) (format "%s>" path)
+               (format "[%s](%s)" contents path))))
+          (`headline
+           (format
+            ;; "[%s](#%s)"
+            "[%s]"
+            ;; Description.
+            (cond ((org-string-nw-p contents))
+                  ((org-export-numbered-headline-p destination info)
+                   (mapconcat #'number-to-string
+                              (org-export-get-headline-number destination info)
+                              "."))
+                  (t (org-export-data (org-element-property :title destination)
+                                      info)))
+            ;; Reference.
+            ;; (or (org-element-property :CUSTOM_ID destination)
+            ;;     (org-export-get-reference destination info))
+            ))
+          (_
+           (let ((description
+                  (or (org-string-nw-p contents)
+                      (let ((number (org-export-get-ordinal destination info)))
+                        (cond
+                         ((not number) nil)
+                         ((atom number) (number-to-string number))
+                         (t (mapconcat #'number-to-string number ".")))))))
+             (when description
+               (format "[%s]"
+                       description
+                       ;; (org-export-get-reference destination info)
+                       )))))))
      ((org-export-inline-image-p link org-html-inline-image-rules)
       (let ((path (let ((raw-path (org-element-property :path link)))
-		    (cond ((not (equal "file" type)) (concat type ":" raw-path))
-			  ((not (file-name-absolute-p raw-path)) raw-path)
-			  (t (expand-file-name raw-path)))))
-	    (caption (org-export-data
-		      (org-export-get-caption
-		       (org-export-get-parent-element link)) info)))
-	(format "![img](%s)"
-		(if (not (org-string-nw-p caption)) path
-		  (format "%s \"%s\"" path caption)))))
+                    (cond ((not (equal "file" type)) (concat type ":" raw-path))
+                          ((not (file-name-absolute-p raw-path)) raw-path)
+                          (t (expand-file-name raw-path)))))
+            (caption (org-export-data
+                      (org-export-get-caption
+                       (org-export-get-parent-element link)) info)))
+        (format "![img](%s)"
+                (if (not (org-string-nw-p caption)) path
+                  (format "%s \"%s\"" path caption)))))
      ((string= type "coderef")
       (let ((ref (org-element-property :path link)))
-	(format (org-export-get-coderef-format ref contents)
-		(org-export-resolve-coderef ref info))))
+        (format (org-export-get-coderef-format ref contents)
+                (org-export-resolve-coderef ref info))))
      ((equal type "radio") contents)
      (t (let* ((raw-path (org-element-property :path link))
-	       (path
-		(cond
-		 ((member type '("http" "https" "ftp" "mailto"))
-		  (concat type ":" raw-path))
-		 ((string= type "file")
-		  (org-export-file-uri (funcall link-org-files-as-md raw-path)))
-		 (t raw-path))))
-	  (if (not contents) (format "%s" path)
-	    (format "*%s* (%s)" contents path)))))))
+               (path
+                (cond
+                 ((member type '("http" "https" "ftp" "mailto"))
+                  (concat type ":" raw-path))
+                 ((string= type "file")
+                  (org-export-file-uri (funcall link-org-files-as-md raw-path)))
+                 (t raw-path))))
+          (if (not contents) (format "%s" path)
+            (format "*%s* (%s)" contents path)))))))
 
 (defun org-slack-verbatim (_verbatim contents _info)
   "Transcode VERBATIM from Org to Slack.
-CONTENTS is the text with bold markup. INFO is a plist holding
-contextual information."
+  CONTENTS is the text with bold markup. INFO is a plist holding
+  contextual information."
   (format "`%s`" contents))
 
 (defun org-slack-code (code _contents info)
   "Return a CODE object from Org to SLACK.
-CONTENTS is nil.  INFO is a plist holding contextual
-information."
+  CONTENTS is nil.  INFO is a plist holding contextual
+  information."
   (format "`%s`"
-	  (org-element-property :value code)))
+          (org-element-property :value code)))
 
 ;;;; Italic
 
 (defun org-slack-italic (_italic contents _info)
   "Transcode italic from Org to SLACK.
-CONTENTS is the text with italic markup.  INFO is a plist holding
-contextual information."
+  CONTENTS is the text with italic markup.  INFO is a plist holding
+  contextual information."
   (format "_%s_" contents))
 
 ;;; Bold
 (defun org-slack-bold (_bold contents _info)
   "Transcode bold from Org to SLACK.
-CONTENTS is the text with bold markup.  INFO is a plist holding
-contextual information."
+  CONTENTS is the text with bold markup.  INFO is a plist holding
+  contextual information."
   (format "*%s*" contents))
 
 
@@ -206,28 +206,28 @@ contextual information."
 
 (defun org-slack-strike-through (_strike-through contents _info)
   "Transcode STRIKE-THROUGH from Org to SLACK.
-CONTENTS is text with strike-through markup.  INFO is a plist
-holding contextual information."
+  CONTENTS is text with strike-through markup.  INFO is a plist
+  holding contextual information."
   (format "~%s~" contents))
 
 
 (defun org-slack-inline-src-block (inline-src-block _contents info)
   "Transcode an INLINE-SRC-BLOCK element from Org to SLACK.
-CONTENTS holds the contents of the item.  INFO is a plist holding
-contextual information."
+  CONTENTS holds the contents of the item.  INFO is a plist holding
+  contextual information."
   (format "`%s`"
-	  (org-element-property :value inline-src-block)))
+          (org-element-property :value inline-src-block)))
 
 ;;;; Src Block
 
 
 (defun org-slack-src-block (src-block contents info)
   "Transcode SRC-BLOCK element into Github Flavored Markdown format.
-CONTENTS is nil. INFO is a plist used as a communication
-channel."
+  CONTENTS is nil. INFO is a plist used as a communication
+  channel."
   (let* ((lang (org-element-property :language src-block))
          (code (org-export-format-code-default src-block info))
-         (prefix (concat "```" lang "\n"))
+         (prefix (concat "```"  "\n"))
          (suffix "```"))
     (concat prefix code suffix)))
 
@@ -235,15 +235,15 @@ channel."
 
 (defun org-slack-quote-block (_quote-block contents info)
   "Transcode a QUOTE-BLOCK element from Org to SLACK.
-CONTENTS holds the contents of the block.  INFO is a plist
-holding contextual information."
+  CONTENTS holds the contents of the block.  INFO is a plist
+  holding contextual information."
   (org-slack--indent-string contents (plist-get info :slack-quote-margin)))
 
 
 (defun org-slack-inner-template (contents info)
   "Return body of document after converting it to Markdown syntax.
-CONTENTS is the transcoded contents string.  INFO is a plist
-holding export options."
+  CONTENTS is the transcoded contents string.  INFO is a plist
+  holding export options."
   ;; Make sure CONTENTS is separated from table of contents and
   ;; footnotes with at least a blank line.
   (concat
@@ -260,8 +260,8 @@ holding export options."
 ;;;; Plain text
 (defun org-slack-plain-text (text info)
   "Transcode a TEXT string into Markdown format.
-TEXT is the string to transcode.  INFO is a plist holding
-contextual information."
+  TEXT is the string to transcode.  INFO is a plist holding
+  contextual information."
   ;; (when (plist-get info :with-smart-quotes)
   ;;   (setq text (org-export-activate-smart-quotes text :html info)))
   ;; The below series of replacements in `text' is order sensitive.
@@ -289,32 +289,32 @@ contextual information."
     (&optional async subtreep visible-only body-only ext-plist)
   "Export current buffer to a text buffer.
 
-If narrowing is active in the current buffer, only export its
-narrowed part.
+  If narrowing is active in the current buffer, only export its
+  narrowed part.
 
-If a region is active, export that region.
+  If a region is active, export that region.
 
-A non-nil optional argument ASYNC means the process should happen
-asynchronously.  The resulting buffer should be accessible
-through the `org-export-stack' interface.
+  A non-nil optional argument ASYNC means the process should happen
+  asynchronously.  The resulting buffer should be accessible
+  through the `org-export-stack' interface.
 
-When optional argument SUBTREEP is non-nil, export the sub-tree
-at point, extracting information from the headline properties
-first.
+  When optional argument SUBTREEP is non-nil, export the sub-tree
+  at point, extracting information from the headline properties
+  first.
 
-When optional argument VISIBLE-ONLY is non-nil, don't export
-contents of hidden elements.
+  When optional argument VISIBLE-ONLY is non-nil, don't export
+  contents of hidden elements.
 
-When optional argument BODY-ONLY is non-nil, strip title and
-table of contents from output.
+  When optional argument BODY-ONLY is non-nil, strip title and
+  table of contents from output.
 
-EXT-PLIST, when provided, is a property list with external
-parameters overriding Org default settings, but still inferior to
-file-local settings.
+  EXT-PLIST, when provided, is a property list with external
+  parameters overriding Org default settings, but still inferior to
+  file-local settings.
 
-Export is done in a buffer named \"*Org SLACK Export*\", which
-will be displayed when `org-export-show-temporary-export-buffer'
-is non-nil."
+  Export is done in a buffer named \"*Org SLACK Export*\", which
+  will be displayed when `org-export-show-temporary-export-buffer'
+  is non-nil."
   (interactive)
   (org-export-to-buffer 'slack "*Org SLACK Export*"
     async subtreep visible-only body-only ext-plist (lambda () (text-mode))))
@@ -324,30 +324,30 @@ is non-nil."
     (&optional async subtreep visible-only body-only ext-plist)
   "Export current buffer to a text file.
 
-If narrowing is active in the current buffer, only export its
-narrowed part.
+  If narrowing is active in the current buffer, only export its
+  narrowed part.
 
-If a region is active, export that region.
+  If a region is active, export that region.
 
-A non-nil optional argument ASYNC means the process should happen
-asynchronously.  The resulting file should be accessible through
-the `org-export-stack' interface.
+  A non-nil optional argument ASYNC means the process should happen
+  asynchronously.  The resulting file should be accessible through
+  the `org-export-stack' interface.
 
-When optional argument SUBTREEP is non-nil, export the sub-tree
-at point, extracting information from the headline properties
-first.
+  When optional argument SUBTREEP is non-nil, export the sub-tree
+  at point, extracting information from the headline properties
+  first.
 
-When optional argument VISIBLE-ONLY is non-nil, don't export
-contents of hidden elements.
+  When optional argument VISIBLE-ONLY is non-nil, don't export
+  contents of hidden elements.
 
-When optional argument BODY-ONLY is non-nil, strip title and
-table of contents from output.
+  When optional argument BODY-ONLY is non-nil, strip title and
+  table of contents from output.
 
-EXT-PLIST, when provided, is a property list with external
-parameters overriding Org default settings, but still inferior to
-file-local settings.
+  EXT-PLIST, when provided, is a property list with external
+  parameters overriding Org default settings, but still inferior to
+  file-local settings.
 
-Return output file's name."
+  Return output file's name."
   (interactive)
   (let ((file (org-export-output-file-name ".txt" subtreep)))
     (org-export-to-file 'slack file
@@ -362,14 +362,12 @@ Return output file's name."
     (kill-new (org-export-as 'slack) ))
   )
 
+;; (org-export-register-backend 'slack)
 (provide 'ox-slack)
 
 ;; Local variables:
-;; generated-autoload-file: "org-loaddefs.el"
+;; generated-autoload-file: "ox-slack-loaddefs.el"
 ;; coding: utf-8
 ;; End:
 
 ;;; ox-slack.el ends here
-
-
-
